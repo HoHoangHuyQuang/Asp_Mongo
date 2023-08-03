cb65fcc3e2823636d75131d144ffa9976f03f54a
HoHoangHuyQuang <huyquang2842@gmail.com>
2023-08-03 09:02:12 +0700

    add multi lauch

--- Api_Gateway/Program.cs
+++ Api_Gateway/Program.cs
@@ -12,16 +12,14 @@ builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
